851f09283781a6088766a46e692d2acde1448195
Durga1534 <durgaprasadpc143@gmail.com>
2026-04-26 19:18:46 +0530

    day4 building simple mongo crud API completed

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/Durga1534/01-web-server
 
 go 1.24.0
 
-
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
---
+++ projects/04-mongo-crud/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/Durga1534/mongo-crud/controllers"
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2"
+)
+
+func main() {
+	r := httprouter.New()
+	uc := controllers.NewUserController(getSession())
+
+	r.GET("/user/:id", uc.GetUser)
+	r.POST("/user", uc.CreateUser)
+	r.DELETE("/user/:id", uc.DeleteUser)
+	http.ListenAndServe("localhost:9000", r)
+}
+
+func getSession() *mgo.Session {
+
+	s, err := mgo.Dial("mongodb://localhost:27107")
+	if err != nil {
+		panic(err)
+	}
+	return s
+}
